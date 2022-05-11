d226509aaceb08f1260fa93138b4cf7110cb3900
buurtijs <delahera.antonio@gmail.com>
2022-05-11 22:32:15 +0200

    updated solidarity payment page
    now there is a better structure, and there are editable fields that interact together to formulate a price!
    
    missing are:
    - links to budget, extended solidarity payments section and payment link
    - new drawing
    - edit text

--- solidarity-payments.js
+++ solidarity-payments.js
@@ -1,87 +1,47 @@
 document.title = "BUURTIJS - Solidarity Payment";
 
 let cnv;
-
-let drawing1;
-
-
-let s;
-let sSize;
-let textX;
-let textY;
-var textWidth;
-let mobBr = 580;
-
-
-function preload (){
-  // drawing1 = loadImage ("images/Bike2-s.png");
-}
-// ADD DRAWING LINK ABOVE
-
+var wage;
+var xTubs;
 
 function setup() {
-  // cnv = createCanvas(windowWidth, 400);
+  cnv = createCanvas(windowWidth, 400);
+  wage = select('#wage');
+  wage.input(updatePayments);
+  xTubs = select('#xtubs')
+  xTubs.input(updatePayments)
 
-// drawing size
-  drawingX = windowWidth;
-
- // s = ('WE ARE ASKING FOR A SOLIDARITY PAYMENT');
- // s2 = ('Under a solidarity model, we do not sell you a product but instead sell you our labour, our work, our time. What wage do you want to give to us? This should be based on your own wage. So if you are a freelancer and you earn €5 an hour, then you can pay us that. But if you earn €80 an hour, then you can pay us that.');
-
-// text size, and adapted for mobile or not
- if (windowWidth <= mobBr) {
-   textX = 30;
-   textY = 0.2*windowHeight;
-   textWidth = windowWidth-60;
-    sSize = 20;
- }
-   else if (windowWidth >= mobBr) {
-     textX = 0.20*windowWidth;
-     textY = 0.30*windowHeight;
-     textWidth = 480;
-      sSize = 25;
-   }
-
+//   createP('€ "input" /hr');
+// createP('1 tub = 15 mins = €... <br> 2 tubs = 30 mins = €... <br> 3 tubs = 45 mins = €... <br> x tubs = x mins = €...')
+// createA('http://p5js.org/', 'link to pay')
 }
 
 function draw() {
-background (33, 33, 33);
-fill('white');
-
-image (drawing1, 0, 0);
- drawing1.resize(drawingX, 0);
-
-textAlign(LEFT)
-textFont('HERAHUGHES', sSize)
-text(s, textX, textY, textWidth, 450);
-text(s2, textX, textY+30, textWidth, 450);
-text(s2, textX, textY+150, textWidth, 450);
-
-
-
-
-
+// background (33, 33, 33);
 }
+
+function updatePayments(){
+priceOne = wage.html() / 4;
+priceTwo = wage.html() / 2;
+priceThree = wage.html()* 0.75;
+tubMins = xTubs.html() * 15;
+priceX = wage.html() * xTubs.html() / 4;
+  select('#onetub').html(priceOne);
+  select('#twotub').html(priceTwo);
+  select('#threetub').html(priceThree);
+  select('#tubmins').html(tubMins);
+  select('#xtub').html(priceX);
+}
+
+// function updatePaymentx(){
+// tubMins = xTubs.html() * 15;
+// priceX = wage.html() * xTubs / 4;
+//   select('#tubmins').html(tubMins);
+//   select('#xtub').html(priceX);
+// }
 
 
 
     function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-
-  if (windowWidth <= mobBr) {
-    textX = 30;
-    textY = 0.2*windowHeight;
-    textWidth = windowWidth-60;
-      sSize = 20;
-  }
-    else if (windowWidth >= mobBr) {
-      textX = 0.20*windowWidth;
-      textY = 0.30*windowHeight;
-      textWidth = 480;
-        sSize = 25;
-    }
-}
-
-function mousePressed () {
-window.open ("archive.html", '_self');
 }

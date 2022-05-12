722ef4cf0c02f241e3dad30fb5e192be5baece05
buurtijs <delahera.antonio@gmail.com>
2022-05-12 12:04:08 +0200

    updated solidarity payments
    all sorts of styling, and links now linked
    
    remaining:
    - pop up of what is?
    - automatic empty of '...'
    - final edit of text

--- solidarity-payments.js
+++ solidarity-payments.js
@@ -10,10 +10,6 @@ function setup() {
   wage.input(updatePayments);
   xTubs = select('#xtubs')
   xTubs.input(updatePayments)
-
-//   createP('€ "input" /hr');
-// createP('1 tub = 15 mins = €... <br> 2 tubs = 30 mins = €... <br> 3 tubs = 45 mins = €... <br> x tubs = x mins = €...')
-// createA('http://p5js.org/', 'link to pay')
 }
 
 function draw() {
@@ -32,15 +28,6 @@ priceX = wage.html() * xTubs.html() / 4;
   select('#tubmins').html(tubMins);
   select('#xtub').html(priceX);
 }
-
-// function updatePaymentx(){
-// tubMins = xTubs.html() * 15;
-// priceX = wage.html() * xTubs / 4;
-//   select('#tubmins').html(tubMins);
-//   select('#xtub').html(priceX);
-// }
-
-
 
     function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
